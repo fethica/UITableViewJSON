26093955ace2d0ce792e74ca5b5af1579ab49f08
Fethi El Hassasna <e.fethi.c@gmail.com>
2013-06-08 00:12:28 +0100

    Add DetailViewController to show character's details

---
+++ UITableViewJSON/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  UITableViewJSON
+//
+//  Created by El Hassasna Fethi on 6/8/13.
+//  Copyright (c) 2013 Fethi El Hassasna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ UITableViewJSON/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  UITableViewJSON
+//
+//  Created by El Hassasna Fethi on 6/8/13.
+//  Copyright (c) 2013 Fethi El Hassasna. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
