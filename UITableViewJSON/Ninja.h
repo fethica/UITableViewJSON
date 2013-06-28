3f642f3ec07ad17f44c57dbbd0c9b582d8cd5d0d
Fethi El Hassasna <e.fethi.c@gmail.com>
2013-06-28 01:14:36 +0100

    Add Ninja class to the project

---
+++ UITableViewJSON/Ninja.h
@@ -0,0 +1,23 @@
+//
+//  Ninja.h
+//  UITableViewJSON
+//
+//  Created by El Hassasna Fethi on 6/27/13.
+//  Copyright (c) 2013 Fethi El Hassasna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ninja : NSObject
+
+@property (strong, nonatomic)NSString *name;
+@property (strong, nonatomic)NSString *thumbnail;
+@property (strong, nonatomic)NSString *photo;
+@property (strong, nonatomic)NSString *description;
+
+- (id)initWithName:(NSString *)aName
+         thumbnail:(NSString *)aThumbnail
+             photo:(NSString *)aPhoto
+       description:(NSString *)aDescription;
+
+@end
