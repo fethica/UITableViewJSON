2eff1b8a8381ea067bf17584221928a077b0221e
Fethi El Hassasna <e.fethi.c@gmail.com>
2014-08-22 15:02:57 +0100

    Add DetailViewController

---
+++ UITableViewJSON/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  UITableViewJSON
+//
+//  Created by El Hassasna Fethi on 8/22/14.
+//  Copyright (c) 2014 Fethi El Hassasna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ UITableViewJSON/DetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.m
+//  UITableViewJSON
+//
+//  Created by El Hassasna Fethi on 8/22/14.
+//  Copyright (c) 2014 Fethi El Hassasna. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
